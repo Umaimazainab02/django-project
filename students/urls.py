4ebd85ce94d3774f9257df9b950fffff83247ead
umaimazainab02 <umaimazainab@429gmail.com>
2024-09-29 14:48:52 +0500

    some messege

---
+++ students/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path("",views.student_list, name="students"),
+    path("all_students/", views.all_students, name="all_students")
+]
--- university_management/urls.py
+++ university_management/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 from . import views
 views.index
@@ -26,4 +26,5 @@ urlpatterns = [
     path('', views.index, name="index"),
     path('about/' , views.about, name="about"),
     path('contact/' , views.contact, name="contact"),
+    path("student/", include('students.urls'))
 ]
